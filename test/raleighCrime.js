c3198fbfd3d63388172c20c2070a5820f7c96389
Donald Heppner <donald.heppner@gmail.com>
2018-02-22 21:05:02 -0500

    Added test for raleighCrime. introduced chai, async with mocha tests

--- test/raleighCrime.js
+++ test/raleighCrime.js
@@ -3,12 +3,16 @@
 const supertest = require('supertest');
 const sinon = require('sinon');
 const nock = require('nock');
+const chai = require('chai');
 const app = require('../app');
 const raleighCrime = require('../controllers/raleighCrime');
+const expect = chai.expect;
 
 const sandbox = sinon.createSandbox();
 
 describe('DataSource Tests', function () {
+    const testDoc = { name: 'test' };
+
     let server = app.listen();
     let request = supertest.agent(server);
 
@@ -16,15 +20,27 @@ describe('DataSource Tests', function () {
         sandbox.restore();
     });
 
-    it('get /raleigh/crime', function (done) {
-        const testDoc = { name: 'test' };
-
+    it('get /raleigh/crime', async() => {
         let scope = nock(raleighCrime.crimeUrl.origin).get(/.*/)
             .reply(200, testDoc);
-
-        request
+        
+        await request
             .get('/raleigh/crime')
+            .expect(200, testDoc);
+
+        let isDone = scope.isDone();
+        expect(scope.isDone()).to.be.true;   // make sure it was called
+    });
+
+    it('get /raleigh/crime with query', async() => {
+        let scope = nock(raleighCrime.crimeUrl.origin).get(/.*abc=123/)
+            .reply(200, testDoc);
+
+        await request
+            .get('/raleigh/crime?query=abc%3D123')
             .expect(testDoc)
-            .expect(200, done);
+            .expect(200);
+
+        expect(scope.isDone()).to.be.true;
     });
 });
